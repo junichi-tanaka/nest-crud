944bc1774b558d2c4ec0f8aea076f402df0b9551
Junichi TANAKA <junsan45@gmail.com>
2021-12-18 00:04:00 +0900

    first migration

--- src/migration/1639752880654-create-user.ts
+++ src/migration/1639752880654-create-user.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class createUser1639752538867 implements MigrationInterface {
-    name = 'createUser1639752538867'
+export class createUser1639752880654 implements MigrationInterface {
+    name = 'createUser1639752880654'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "firstName" varchar NOT NULL, "lastName" varchar NOT NULL, "age" integer NOT NULL, "isActive" boolean NOT NULL DEFAULT (1))`);
